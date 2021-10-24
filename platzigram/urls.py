274e33e36b2df6e26cdbf0aa2470a5196bf44c39
Rafael Livise <r.livise@hotmail.com>
2021-10-24 10:15:33 -0500

    feat:migrations user and post , admin panel modifi

---
+++ platzigram/urls.py
@@ -0,0 +1,18 @@
+"""Modulo de urls django"""
+# Django
+from django.urls import path
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+from platzigram import views as local_views
+from posts import views as posts_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('hello-word/', local_views.hello_world),
+    path('sorted/', local_views.sort_integers),
+    path('hi/<str:name>/<int:age>', local_views.say_hi),
+
+    path('posts/', posts_views.list_posts)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ posts/apps.py
@@ -0,0 +1,8 @@
+"""Posts aplication module."""
+from django.apps import AppConfig
+
+
+class PostsConfig(AppConfig):
+    """Posts aplications settings"""
+    name = 'posts'
+    verbose_name = "Posts"
